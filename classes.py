4852f70a9a022184230a2627919f147358bc27db
Dmitriy <astrovskiy.04@gmail.com>
2021-08-11 22:10:36 +0300

    piece class created

--- classes.py
+++ classes.py
@@ -1,64 +1,11 @@
-class Cell:
-    def __init__(self, pos_x, pos_y, bg_color):
-        self.posX = pos_x
-        self.posY = pos_y
-        self.bg_color = bg_color
-
-
-class Figure(Cell):
-    possible_moves = []
-
-    def __init__(self, pos_x, pos_y, bg_color, color):
-        super().__init__(pos_x, pos_y, bg_color)
+class Piece:
+    def __init__(self, pos_x, pos_y, color, figure_name, img_folder):
         self.color = color
+        self.pos_x = int(pos_x)
+        self.pos_y = int(pos_y)
+        self.figure_name = figure_name
+        self.img = img_folder+"/{}.png".format(color+figure_name)
+        self.dead = False
 
     def move(self):
         pass
-
-
-class Pawn(Figure):
-    possible_moves = ["y+1", "y+2", "y+1.x+1"]
-
-    def __init__(self, pos_x, pos_y, bg_color, color):
-        super().__init__(pos_x, pos_y, bg_color, color)
-
-
-class Bishop(Figure):
-    possible_moves = ["xy"]
-
-    def __init__(self, pos_x, pos_y, bg_color, color):
-        super().__init__(pos_x, pos_y, bg_color, color)
-
-
-class Knight(Figure):
-    possible_moves = ["y2x1", 'x2y1']
-
-    def __init__(self, pos_x, pos_y, bg_color, color):
-        super().__init__(pos_x, pos_y, bg_color, color)
-
-
-class Rook(Figure):
-    possible_moves = ["x+", "y+"]
-
-    def __init__(self, pos_x, pos_y, bg_color, color):
-        super().__init__(pos_x, pos_y, bg_color, color)
-
-
-class Queen(Figure):
-    possible_moves = ["x+", "y+", "x"]
-
-    def __init__(self, pos_x, pos_y, bg_color, color):
-        super().__init__(pos_x, pos_y, bg_color, color)
-
-
-class King(Figure):
-    def __init__(self, pos_x, pos_y, bg_color, color):
-        super().__init__(pos_x, pos_y, bg_color, color)
-
-
-class Board:
-    def __init__(self):
-        self.width = 800
-        self.height = 800
-        self.vertical_positions = ["1", "2", "3", "4", "5", "6", "7", "8"]
-        self.horizontal_positions = ["A", "B", "C", "D", "E", "F", "G", "H"]
