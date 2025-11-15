60eeb73c4590f2cce9809d2b286796e803e3606e
geekchanskiy <astrovskiy.04@gmail.com>
2025-11-15 22:46:11 +0300

    ruff added

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 """
-    Chess game written by DMT
-    https://github.com/GeekchanskiY/CHESS
+Chess game written by DMT
+https://github.com/GeekchanskiY/CHESS
 
-    base api for chess experiments
+base api for chess experiments
 
-    Future plan:
-        - Finish piece movement logic
-        - add "en passant" rule
+Future plan:
+    - Finish piece movement logic
+    - add "en passant" rule
 
-        - add AI
-        - add chess.com support for AI learning by analyzing games
-        - Create vk api for playing with friends
-        - have fun :)
-
-
+    - add AI
+    - add chess.com support for AI learning by analyzing games
+    - Create vk api for playing with friends
+    - have fun :)
 """
 
 import pygame
@@ -58,28 +56,47 @@ piece_names = {
     "Q": "Queen",
     "R": "Rook",
     "B": "Bishop",
-    "N": "Knight"
+    "N": "Knight",
 }
 
 # Const for first creating pieces
 start_positions = [
-        'wP12', 'wP22', "wP32", "wP42", "wP52", "wP62", "wP72", "wP82",
-        'wR11', "wN21", "wB31", "wQ41", "wK51", "wB61", "wN71", "wR81",
-        'bP17', 'bP27', "bP37", "bP47", "bP57", "bP67", "bP77", "bP87",
-        'bR18', "bN28", "bB38", "bQ48", "bK58", "bB68", "bN78", "bR88",
-    ]
+    "wP12",
+    "wP22",
+    "wP32",
+    "wP42",
+    "wP52",
+    "wP62",
+    "wP72",
+    "wP82",
+    "wR11",
+    "wN21",
+    "wB31",
+    "wQ41",
+    "wK51",
+    "wB61",
+    "wN71",
+    "wR81",
+    "bP17",
+    "bP27",
+    "bP37",
+    "bP47",
+    "bP57",
+    "bP67",
+    "bP77",
+    "bP87",
+    "bR18",
+    "bN28",
+    "bB38",
+    "bQ48",
+    "bK58",
+    "bB68",
+    "bN78",
+    "bR88",
+]
 
 # Dictionaries for translating numeric pos name to classic and vice versa
-field_names_x = {
-    "A": 1,
-    "B": 2,
-    "C": 3,
-    "D": 4,
-    "E": 5,
-    "F": 6,
-    "G": 7,
-    "H": 8
-}
+field_names_x = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8}
 field_names_x_rev = {value: key for key, value in field_names_x.items()}
 
 
@@ -89,10 +106,15 @@ positions = {}
 # Generating coordinates
 for z in range(1, 9):
     for i in range(1, 9):
-        positions[int(str(z) + str(9-i))] = [size * z, size * (z+1), size * i, size * (i+1)]
+        positions[int(str(z) + str(9 - i))] = [
+            size * z,
+            size * (z + 1),
+            size * i,
+            size * (i + 1),
+        ]
 
 # Image folders
-img_folder = (os.path.abspath("images/berlin/"))
+img_folder = os.path.abspath("images/berlin/")
 dot_img = os.path.abspath("images/dot.png")
 
 
@@ -138,16 +160,23 @@ def pawn_move(instance):
                 if piece.pos == instance.pos - 1:
                     f1 = False
 
-
         # En passant rule
         if piece.pos == instance.pos + 10:
-            if piece.name == "P" and piece.last_turn == turn - 1 and piece.color != instance.color:
+            if (
+                piece.name == "P"
+                and piece.last_turn == turn - 1
+                and piece.color != instance.color
+            ):
                 if instance.color == "w":
                     moves.append([instance.pos + 11, piece])
                 else:
                     moves.append([instance.pos + 9, piece])
         if piece.pos == instance.pos - 10:
-            if piece.name == "P" and piece.last_turn == turn - 1 and piece.color != instance.color:
+            if (
+                piece.name == "P"
+                and piece.last_turn == turn - 1
+                and piece.color != instance.color
+            ):
                 if instance.color == "w":
                     moves.append([instance.pos - 9, piece])
                 else:
@@ -412,7 +441,6 @@ def bishop_move(instance):
                         found = True
                         moves.append([piece.pos, piece])
             if not found:
-
                 moves.append([temp_pos + 9, None])
                 temp_pos += 9
 
@@ -507,26 +535,29 @@ def knight_move(instance):
 def draw_hints(instance):
     # Hint drawing for each possible move of selected piece. Also useful for creating logic.
     if instance.moves is not None and len(instance.moves) > 0:
-
         for move in instance.moves:
-            window.blit(pygame.image.load(dot_img), (positions.get(move[0])[0], positions.get(move[0])[2]))
+            window.blit(
+                pygame.image.load(dot_img),
+                (positions.get(move[0])[0], positions.get(move[0])[2]),
+            )
     else:
         print("I cant move :(")
 
 
 class Piece:
     """
-        Main dynamic piece class with all it's actions
+    Main dynamic piece class with all it's actions
 
-        Color, name, dead and position are for rendering and logic
+    Color, name, dead and position are for rendering and logic
 
-        last_turn is an argument for AI and pawn logic (1st move & en passant rule)
-        To read more about the "en passant" rule you can visit:
-            https://en.wikipedia.org/wiki/En_passant
+    last_turn is an argument for AI and pawn logic (1st move & en passant rule)
+    To read more about the "en passant" rule you can visit:
+        https://en.wikipedia.org/wiki/En_passant
 
-        img argument is for holding rendering image only
+    img argument is for holding rendering image only
 
     """
+
     def __init__(self, color, name, pos):
         self.color = color
         self.name = name
@@ -575,8 +606,12 @@ class Piece:
         print(current_color)
         moved = False
         for move in self.moves:
-
-            if self.color == "w" and current_color == 0 or self.color == "b" and current_color == 1:
+            if (
+                self.color == "w"
+                and current_color == 0
+                or self.color == "b"
+                and current_color == 1
+            ):
                 if pos == move[0]:
                     moved = True
                     if current_color == 0:
@@ -640,9 +675,15 @@ def draw_board():
     for i in range(1, 9):
         for z in range(1, 9):
             if cnt % 2 == 0:
-                pygame.draw.rect(window, pygame.Color(50, 50, 50), (size*z, size*i, size, size))
+                pygame.draw.rect(
+                    window, pygame.Color(50, 50, 50), (size * z, size * i, size, size)
+                )
             else:
-                pygame.draw.rect(window, pygame.Color(255, 255, 255), (size*z, size*i, size, size))
+                pygame.draw.rect(
+                    window,
+                    pygame.Color(255, 255, 255),
+                    (size * z, size * i, size, size),
+                )
             cnt += 1
         cnt -= 1
 
@@ -684,6 +725,7 @@ def draw_figures():
     # Rendering of each piece
     for piece in pieces:
         window.blit(pygame.image.load(piece.img), (piece.get_pos_coord()))
+
 
 # game init and window options
 pygame.init()
