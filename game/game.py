48f3446f89cefa7634d53dbf4bc0e98645dd8d1f
geekchanskiy <astrovskiy.04@gmail.com>
2026-03-03 22:08:03 +0300

    annotations added, format improved

--- game/game.py
+++ game/game.py
@@ -1,11 +1,16 @@
 from itertools import product
 import pygame
+from typing import Dict
 
 from .board import Board
 from .assets import Assets
 
 
 class Tile:
+    """
+    Tile is a class which represents one cell on the board, and contains info about its position on screen and color
+    """
+
     def __init__(self, pos: int, width: int, offset_x: int, offset_y: int, color: pygame.Color):
         self.pos = pos
 
@@ -17,22 +22,21 @@ class Tile:
         self.offset_y = offset_y
         self.color = color
 
-    def get_rect(self) -> set[int, int, int, int]:
-        return (self.pos_x, self.pos_y, self.width, self.width)
+    def get_rect(self) -> pygame.Rect:
+        """Returns coordinates of tile in pixels"""
+        return pygame.Rect(self.pos_x, self.pos_y, self.width, self.width)
 
     def get_color(self):
+        """Returns color of tile"""
         return self.color
 
     def pos_matches(self, pos: int) -> bool:
+        """Returns true if tile position matches given position"""
         return self.pos == pos
 
-    def get_piece_coords(self) -> set[int]:
-        return (
-            self.pos_x,
-            self.pos_y,
-            self.pos_x + self.width,
-            self.pos_y + self.width,
-        )
+    def get_piece_coords(self) -> pygame.Rect:
+        """Gets coordinates of piece on tile in pixels"""
+        return pygame.Rect(self.pos_x, self.pos_y, self.width, self.width)
 
 
 class Game:
@@ -44,52 +48,51 @@ class Game:
     modes = {}
 
     def __init__(self):
-        self._is_running = True
-        self._cell_size = 80
+        self._is_running: bool = True
+        self._cell_size: int = 80
 
         # Pygame config
         pygame.init()
         pygame.display.set_caption("CHESS")
         pygame.display.set_mode((self._cell_size * 8, self._cell_size * 8))
 
-        self.surface = pygame.display.get_surface()
+        self.surface: pygame.Surface = pygame.display.get_surface()
         self.surface.fill((211, 211, 211))
-        self._offset_x = 0
-        self._offset_y = 0
+        self._offset_x: int = 0
+        self._offset_y: int = 0
 
         # Board config
         self.board: Board = Board()
-        self.assets = Assets().load("pixel")
+        self.assets: Assets = Assets().load("pixel")
 
         # Tiles
         self.tiles: list[Tile] = []
-        self.tiles_dict = {}
+        self.tiles_dict: Dict[int, Tile] = {}
+
         for i, z in product(range(1, 9), range(1, 9)):
-            new_tile = Tile(
+            new_tile: Tile = Tile(
                 i * 10 + z,
                 self._cell_size,
                 self._offset_x,
                 self._offset_y,
                 pygame.Color(50, 50, 50) if (i + z) % 2 == 0 else pygame.Color(205, 205, 205),
             )
+
             self.tiles.append(new_tile)
             self.tiles_dict[int(str(i) + str(9 - z))] = new_tile
 
     def _draw_pieces(self):
-        """
-        Draws board pieces
-        """
+        """Draws board pieces"""
 
         for piece in self.board.pieces:
-            self.surface.blit(pygame.image.load(self.assets.get_piece_image(piece)), (self._get_pos(piece.get_pos())))
+            self.surface.blit(pygame.image.load(self.assets.get_piece_image(piece)), self._get_pos(piece.get_pos()))
 
     def _get_pos(self, pos: int) -> set[int]:
+        """Get position of tile in pixels"""
         return self.tiles_dict[pos].get_piece_coords()
 
     def _draw(self):
-        """
-        Draws tiles and pieces
-        """
+        """Draws tiles and pieces"""
         for i in self.tiles:
             pygame.draw.rect(
                 self.surface,
@@ -100,25 +103,32 @@ class Game:
         self._draw_pieces()
 
     def _mouse_pos(self) -> int:
-        """
-        Returns tile number from current mouse coordinates
-        """
+        """Returns tile number from current mouse coordinates"""
 
-        mouse_position = pygame.mouse.get_pos()
-        mouse_x = mouse_position[0]
-        mouse_y = mouse_position[1]
-        coord_x = (mouse_x - self._offset_x) // self._cell_size + 1
+        mouse_position: tuple[int, int] = pygame.mouse.get_pos()
+        mouse_x: int = mouse_position[0]
+        mouse_y: int = mouse_position[1]
 
+        coord_x: int = (mouse_x - self._offset_x) // self._cell_size + 1
         if coord_x > 8:
             return None
 
-        coord_y = (mouse_y - self._offset_y) // self._cell_size + 1
+        coord_y: int = (mouse_y - self._offset_y) // self._cell_size + 1
         if coord_y > 8:
             return None
 
         return coord_x + coord_y * 10
 
+    def _get_piece_on_tile(self, pos: int):
+        """Returns piece on tile with given position, or None if there is no piece"""
+        for piece in self.board.pieces:
+            if piece.get_pos() == pos:
+                return piece
+
+        return None
+
     def run(self):
+        """Game loop, which handles user input and renders window"""
         while self._is_running:
             pygame.time.delay(100)
 
@@ -128,6 +138,8 @@ class Game:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:  # LMB
                         print("LMB", self._mouse_pos())
+                        piece = self._get_piece_on_tile(self._mouse_pos())
+                        print(piece.get_name() if piece else "no piece")
                     elif event.button == 3:  # RMB
                         print("RMB", self._mouse_pos())
 
--- pieces/pawn.py
+++ pieces/pawn.py
@@ -9,12 +9,12 @@ class Pawn(Piece):
         if type(color) is not Color:
             raise InvalidColorException()
 
-        self.pos = pos
+        self.pos: int = pos
         self.moves: list[Move] = []
-        self.color = color
-        self.name = "P"
+        self.color: Color = color
+        self.name: str = "P"
 
-        self.VALUE = 1
+        self.VALUE: int = 1
 
     def move(self, pos: int, other_pieces: list[Piece]):
         if pos not in self.get_available_moves(other_pieces):
@@ -26,64 +26,6 @@ class Pawn(Piece):
 
     def get_available_moves(self, other_pieces: list[Piece]) -> list[int]:
         moves = []
-        f1 = True
-        f2 = True
-        for piece in other_pieces:
-            if self.color == WHITE:
-                if piece.pos == self.pos + 11 and piece.color != self.color:
-                    moves.append([piece.pos, piece])
-                if piece.pos == self.pos - 9 and piece.color != self.color:
-                    moves.append([piece.pos, piece])
-                if self.last_turn is None:
-                    if piece.pos == self.pos + 1:
-                        f1 = False
-                        f2 = False
-                    if piece.pos == self.pos + 2:
-                        f2 = False
-                else:
-                    if piece.pos == self.pos + 1:
-                        f1 = False
-                    f2 = False
-            else:
-                if piece.pos == self.pos + 9 and piece.color != self.color:
-                    moves.append([piece.pos, piece])
-                if piece.pos == self.pos - 11 and piece.color != self.color:
-                    moves.append([piece.pos, piece])
-                if self.last_turn is None:
-                    if piece.pos == self.pos - 1:
-                        f1 = False
-                        f2 = False
-                    if piece.pos == self.pos - 2:
-                        f2 = False
-                else:
-                    f2 = False
-                    if piece.pos == self.pos - 1:
-                        f1 = False
-
-            # En passant rule
-            if piece.pos == self.pos + 10:
-                if piece.name == "P" and piece.last_turn == turn - 1 and piece.color != instance.color:
-                    if self.color == "w":
-                        moves.append([self.pos + 11, piece])
-                    else:
-                        moves.append([self.pos + 9, piece])
-            if piece.pos == self.pos - 10:
-                if piece.name == "P" and piece.last_turn == turn - 1 and piece.color != self.color:
-                    if self.color == "w":
-                        moves.append([self.pos - 9, piece])
-                    else:
-                        moves.append([self.pos - 11, piece])
-
-        if f1:
-            if self.color == WHITE:
-                moves.append([self.pos + 1, None])
-            else:
-                moves.append([self.pos - 1, None])
-        if f2:
-            if self.color == WHITE:
-                moves.append([self.pos + 2, None])
-            else:
-                moves.append([self.pos - 2, None])
 
         return moves
 
